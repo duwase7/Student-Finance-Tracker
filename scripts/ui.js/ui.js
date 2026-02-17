efb480b606407adf48edfed8610de20ef59e8f9e
duwase7 <d.uwase7@alustudent.com>
2026-02-18 00:47:35 +0200

    Connected buttons

--- scripts/ui.js/ui.js
+++ scripts/ui.js/ui.js
@@ -5,3 +5,21 @@ export function highlight(text, re) {
      return text;
     }
 }
+import { exportTransactions, importTransactions } from "./storage.js";
+import { transactions, setTransactions } from "./state.js";
+
+document.getElementById("export-json")
+    .addEventListener("click", () => {
+        exportTransactions(transactions);
+    });
+
+document.getElementById("import-json")
+    .addEventListener("change", (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            importTransactions(file, (data) => {
+                setTransactions(data);
+                renderTable();
+            });
+        }
+    });
